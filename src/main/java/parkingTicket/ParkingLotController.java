2ebf5d414523cab5b8f938c7d1d9e73a07967440
maylover55 <64656963+maylover55@users.noreply.github.com>
2020-07-01 02:16:19 +0900

    Update ParkingLotController.java

--- src/main/java/parkingTicket/ParkingLotController.java
+++ src/main/java/parkingTicket/ParkingLotController.java
@@ -8,14 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
-import org.springframework.beans.BeanUtils;
+
 
  @RestController
  public class ParkingLotController {
-     // 서킷브레이커 테스트를 위한 동기 호출
-     @RequestMapping(method= RequestMethod.GET, path="/parkingLots/syncCall")
-     public void getSyncCall (){
-         System.out.println("##### ParkingLotController getSyncCall: ");
-         Application.applicationContext.getBean(parkingTicket.external.DeliveryCancelationService.class).syncCall();
-     }
+
  }
